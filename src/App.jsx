007c924825bcc0290df8e525e248078fe9b02618
KABIR <20001001054kabir@dcrustm.org>
2024-04-17 13:55:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-    <div className="bg-[#436f70] min-h-screen py-8">
+    <div className="bg-[#FFB6C1] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Tasks</h1>
                     <div className="mb-4">
